0f1d711c8d565b7fc3b800d0513b9df1b4fae688
sadat khan <khanisadat834@gmail.com>
2024-11-29 22:35:23 +0500

    adding my ruby api

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [ :index, :create, :destroy ]
+    end
+  end
+end
